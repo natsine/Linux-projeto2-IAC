56b9c3408c06e1eac34643daf38a4a0c812a0f5b
natsine <nataliasinegit@gmail.com>
2022-10-29 20:18:09 -0300

    Script para upload do site

---
+++ script-iac2.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+apt install apache2 -y
+apt install unzip -y
+
+
+cd /tmp
+
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+
+cd linux-site-dio-main
+
+cp -R * /var/www/html/
+
+
